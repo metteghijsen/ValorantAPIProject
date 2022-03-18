c6c89fc98b8a1ad5a67f6e41122c58c26dd221f4
I481695 <mette.ghijsen@student.fontys.nl>
2022-03-18 13:41:51 +0100

    audio van karakters toegevoegd

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ fetch("https://valorant-api.com/v1/agents?isPlayableCharacter=true").then((respo
     const agents_element = document.getElementById("agents");
 
     agents.forEach((agent) => {
+        console.log(agent["voiceLine"]["mediaList"][0]["wwise"]);
         console.log(agent);
-        //Hoofdelement aanmaken character
+        //hoofdelement aanmaken character
         const agent_element = document.createElement("div");
-        //H2 voor de naam aanmaken
+
+        //h2 voor de naam aanmaken
         const agent_name_element = document.createElement("h2");
         agent_name_element.innerHTML = agent["displayName"];
 
@@ -27,12 +29,27 @@ fetch("https://valorant-api.com/v1/agents?isPlayableCharacter=true").then((respo
         const agent_description_element = document.createElement("p");
         agent_description_element.innerHTML = agent["description"];
 
+        //audio element aanmaken voor voiceline
+        const agent_audio_element = document.createElement("audio");
+        agent_audio_element.controls = true;
+        //source aanmaken dat in audio element komt
+        const agent_audio_source_element = document.createElement("source");
+        agent_audio_source_element.setAttribute("src",agent["voiceLine"]["mediaList"][0]["wave"]);
+
         //Alles toevoegen aan de HTML
         agent_element.appendChild(agent_image_element);
         agent_element.appendChild(agent_background_element);
         agent_element.appendChild(agent_name_element);
-        agent_element.appendChild(agent_description_element)
+        agent_element.appendChild(agent_description_element);
         agents_element.appendChild(agent_element);
+
+        //audio source toevoegen aan audio element
+        agent_audio_element.appendChild(agent_audio_source_element);
+        agent_element.appendChild(agent_audio_element);
+
+        // const agent_ability_images = document.createElement("img")
+        // agent_ability_images.setAttribute("src",agent["abilities, displayIcon"])
+        // agent_element.appendChild(agent_ability_images)
     })
 })
 
